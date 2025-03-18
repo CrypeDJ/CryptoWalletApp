e06a010edd7b9391ec47d30d3c200916f7c5b060
CrypeDJ <litowrog@gmail.com>
2025-03-18 17:53:08 +0300

    Fix

--- app/src/main/java/com/crype/cryptoapp/presentation/di/AppModule.kt
+++ app/src/main/java/com/crype/cryptoapp/presentation/di/AppModule.kt
@@ -1,8 +1,9 @@
 package com.crype.cryptoapp.presentation.di
 
 import com.crype.cryptoapp.presentation.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    single { MainViewModel() }
+    viewModel { MainViewModel() }
 }
